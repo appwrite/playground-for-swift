78db593adfd6a875e133dbc7b882a1b80521e817
Jake Barnby <jakeb994@gmail.com>
2022-11-04 20:57:56 +1300

    Update version

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(name: "Appwrite", url: "https://github.com/appwrite/sdk-for-swift", .exact("1.0.0"))
+        .package(name: "Appwrite", url: "https://github.com/appwrite/sdk-for-swift", .exact("1.1.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
